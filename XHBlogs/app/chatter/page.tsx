f9e2e73f1d9bc82fb0c57a9be219e2ccb374a08f
fedbfreg <revuyanding@outlook.com>
2026-05-20 02:38:27 +0800

    Update page.tsx

--- XHBlogs/app/chatter/page.tsx
+++ XHBlogs/app/chatter/page.tsx
@@ -8,18 +8,18 @@ import { siteConfig } from '@/siteConfig';
 
 
 export const metadata = {
-  : "杂谈 | "+ siteConfig.
-  描述: “日常碎片与灵感记录”,
+  title: "杂谈 | "+ siteConfig.title,
+  description: "日常碎片与灵感记录",
 };
 
-导出 默认 函数 ChatterPage() {
+export default function ChatterPage() {
   // 注意：这里我们假设你的 md 文件放在根目录的 chatters 文件夹里
   const chattersDirectory = path.join(process.cwd(), 'chatters');
-  let chatters: any[] = [];
+  let chatters = [];
 
-  尝试 {
+  try {
     // 确保文件夹存在
-    如果 (!fs.existsSync(chattersDirectory)) {
+    if (!fs.existsSync(chattersDirectory)) {
       fs.mkdirSync(chattersDirectory);
     }
 
@@ -31,13 +31,13 @@ export const metadata = {
       const { data, content } = matter(fileContents);
 
       return {
-        别名,
+        slug,
         title: data.title || '',
         date: data.date || '未知时间',
-        标签: 数据。标签 || [],
-        心情: 数据。心情 || '',
-        封面: 数据。封面 || '',
-        内容：内容。替换(/^#+ .* /m, '') // 去除开头的 markdown 标题以优化截取显示
+        tags: data.tags || [],
+        mood: data.mood || '',
+        cover: data.cover || '',
+        content: content.replace(/^#+ .*\n/m, '') // 去除开头的 markdown 标题以优化截取显示
       };
     }).sort((a, b) => (new Date(b.date).getTime() - new Date(a.date).getTime())); // 按时间倒序
   } catch (e) {
